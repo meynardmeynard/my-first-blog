cd608f07d190c026e80ee502b8093ba46694d7a1
Jericho Mendoza <jerichomm6@gmail.com>
2016-10-13 06:26:26 +0000

    Extension Tutorial Homework

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
+import django.contrib.auth.views
 from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
+    
 ]
